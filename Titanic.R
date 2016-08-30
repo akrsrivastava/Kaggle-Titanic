ef24962b8f05d27359a14257122132b5712379d7
akrsrivastava <akrsrivastava@yahoo.com>
2016-08-30 19:02:30 +0530

    Created rmd  with this version

--- Titanic.R
+++ Titanic.R
@@ -10,12 +10,12 @@ test <- read.csv("D:/amit/Data Science/Kaggle/Titanic/test.csv", stringsAsFactor
 
 train$IsTrain <- TRUE
 test$IsTrain <- FALSE
-test$Survived <- 0
+test$Survived <- NA
 combi <- rbind(train,test)
 str(combi)
 
 combi$Survived <- as.factor(combi$Survived)
-combi$Sex <- factor(combi$Sex)
+combi$Sex <- as.factor(combi$Sex)
 #Check blanks
 sapply(combi,function(x){sum(ifelse(str_trim(x)=="",1,0))}) #Check no. of blanks in the features
 #1014 Cabin blank, 2 Embarked blank
@@ -34,6 +34,13 @@ ggplot(combi) +geom_boxplot(aes(x=factor(Embarked),y=Fare)) + facet_wrap("Pclass
 #Lets impute Embarked with the most common Embarked value i.e C
 combi[combi$Embarked=="","Embarked"] ="C"
 
+#Check NA in Fare
+combi[which(is.na(combi$Fare)),] 
+#Lets check if there is some other passenger with the same ticket number
+combi[combi$Ticket=="3701",]
+#No one else
+#Lets check the median fare for PClass=3 and Embarked=S
+combi[which(is.na(combi$Fare)),"Fare"] <- median(combi[combi$Pclass==3 & combi$Embarked=="S","Fare"],na.rm=TRUE)
 #Lets check Age NAs now
 
 #Extract Titles
@@ -49,7 +56,7 @@ combi[!combi$Title %in% c("Mr","Mrs","Miss","Master"),"Title"] <- "Royalty"
 table(combi$Title)
 
 
-ggplot(combi[combi$Title=="Mrs",]) + geom_density(aes(x=Age))
+ggplot(combi) + geom_density(aes(x=Age)) + facet_grid("Title")
 
 #We will impute the baisis of Title in the name
 table(combi$Title, is.na(combi$Age))
@@ -93,7 +100,8 @@ Title_survived_df <- as.data.frame(Title_survived_df)
 ggplot(Title_survived_df) + geom_bar(aes(x=Title,y=Freq, fill=Survived),stat="identity" )
 
 
-ggplot(combi) + geom_histogram(aes(x=Age, fill=Survived),binwidth=10,fill="orange",color="black",breaks=seq(0,90,5))
+ggplot(subset(combi,combi$IsTrain==TRUE))  + geom_density(aes(x=Age))
+ggplot(subset(combi,combi$IsTrain==TRUE))  + geom_density(aes(x=Age,col=Survived )) 
 
 #CAtegorize Age into different bins 0-20 20-40 40-60 60-80
 fn_agecat <- function (x){
@@ -142,7 +150,7 @@ df_for_boruta <- df_for_boruta[,!names(df_for_boruta) %in% c("Sex","Embarked","T
 df_for_boruta <- bind_cols(df_for_boruta,df_factors)
 
 #boruta_features <- Boruta(data=df_for_boruta,Survived~.,doTrace=2)
-boruta_features$finalDecision
+#boruta_features$finalDecision
 
 #All features are deemed important
 # Pclass         SibSp         Parch          Fare      Sex.male    Embarked.Q    Embarked.S 
@@ -153,9 +161,9 @@ boruta_features$finalDecision
 #Set up XGBoost
 #We will use the dataframe created for Boruta as it is already dummyfied and non essential features have 
 #been removed
-#train_xgBoost <- df_for_boruta
-# train_xgBoost <- df_for_boruta[-c(1:99),]
-# test_xgBoost<- df_for_boruta[1:99,] 
+train_xgBoost <- df_for_boruta
+#train_xgBoost <- df_for_boruta[-c(1:99),]
+#test_xgBoost<- df_for_boruta[1:99,]
 train_xgBoost_Survived_label <- train_xgBoost$Survived
 #train_xgBoost_Survived_label <- as.numeric(levels(train_xgBoost_Survived_label))[train_xgBoost_Survived_label]
 #http://stackoverflow.com/questions/37041943/troubleshooting-xgboost-in-r
@@ -234,7 +242,7 @@ Sys.time()
 # Survived_Predict <- ifelse(Survived_Predict<0.5,1,0)
 # 
 #Prepare test data set, same as the train data set was prepared
-
+xgb_train_1
 xgb_test <- subset(combi,combi$IsTrain==FALSE)
 xgb_test$Age <- NULL
 xgb_test$Ticket <- NULL
@@ -250,7 +258,7 @@ df_factors <- data.frame(predict(dmy,df_factors))
 xgb_test <- xgb_test[,!names(xgb_test) %in% c("Sex","Embarked","Title","AgeCategory")]
 xgb_test <- bind_cols(xgb_test,df_factors)
 xgb_test$Survived <- NULL
-xgb_test <- xgb.DMatrix(data.matrix(xgb_test))
+xgb_test <- data.matrix(xgb_test)
 Survived_Predict <- predict(xgb_train_1$finalModel,xgb_test)
 Survived_Predict <- ifelse(Survived_Predict<=0.5,1,0) #Survived_Predict is the probability of the first level i.e 0.
 
